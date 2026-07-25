6188392ce88dc919ffe6daff9316f56b7b0529a6
kingership321 <kingership321@gmail.com>
2026-07-25 16:03:14 +0545

    fix 405 error1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -60,7 +60,7 @@ router.post('/signup', async (req, res) => {
 });
 
 // Sign in
-router.post('/auth', async (req, res) => {
+router.post('/api/auth', async (req, res) => {
   const { email, password } = req.body;
   console.log('Login attempt:', email);
   try {
